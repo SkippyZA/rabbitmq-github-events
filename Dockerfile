b6b60a9953b1d15f4d716b9a7675f23cd4c85e02
Steven Inskip <inskips@gmail.com>
2016-06-05 01:03:36 +0200

    RabbitMQ and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,14 @@
 FROM node:5.11
 
-ENV NODE_ENV=production
+ENV NODE_ENV production
 
-npm install --only=prod
+WORKDIR /app
+
+ADD ./src/* /app/src/
+ADD package.json /app/
+
+RUN \
+  npm install --only=prod
 
 EXPOSE 8080
 CMD [ "npm", "start" ]
--- src/app.js
+++ src/app.js
@@ -5,60 +5,83 @@
  */
 const rx = require('rx');
 const R = require('ramda');
+const RxAmqplib = require('rx-amqplib');
 const bodyParser = require('body-parser');
 const moment = require('moment');
 const restify = require('restify');
 const config = require('./config');
 
 const app = restify.createServer();
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: '50mb' }));
+
+// Init rabbitmq connection and channel
+const amqpConnection$ = RxAmqplib.newConnection(config.RABBITMQ_HOST);
+const channel$ = amqpConnection$
+  .flatMap(R.invoker(0, 'createChannel'))
+  .doOnNext(() => console.log('Created new rabbitmq channel'));
+
+// Assert a direct routing exchange on rabbitmq
+const exchange$ = channel$
+  .flatMap(channel => channel
+    .assertExchange(config.RABBITMQ_EXCHANGE, config.RABBITMQ_EXCHANGE_TYPE, { durable: false }))
+  .doOnNext(() => console
+    .log({ exchange: config.RABBITMQ_EXCHANGE, exchange_type: config.RABBITMQ_EXCHANGE_TYPE }, 'Asserted exchange'))
+  .shareReplay();
 
 // time => ISO date string
-let isoDate = t => moment(t).toISOString();
+const isoDate = t => moment(t).toISOString();
+const githubEventType = R.prop('x-github-event');
 // headers => github headers
-let githubHeaders = R.pick(['x-github-event', 'x-github-delivery']);
+const githubHeaders = R.pick(['x-github-event', 'x-github-delivery']);
 // request obj => headers + body
-let getShit = R.pick(['headers', 'body']);
+const getShit = R.pick(['headers', 'body']);
 // req => boolean
-let isPushEvent = R.compose(R.propEq('x-github-event', 'push'), githubHeaders, R.prop('headers'));
+const isPushEvent = R.compose(R.propEq('x-github-event', 'push'), githubHeaders, R.prop('headers'));
 // Fixes repository dates for repository
-let evolveDatesToIso = R.evolve({
-    repository: {
-        created_at: isoDate,
-        pushed_at: isoDate
-    }
+const evolveDatesToIso = R.evolve({
+  repository: {
+    created_at: isoDate,
+    pushed_at: isoDate
+  }
 });
 
-// 
-let evolvePushRequest = R.evolve({
-    headers: githubHeaders,
-    body: evolveDatesToIso
+// Fixes request
+const evolvePushRequest = R.evolve({
+  headers: githubHeaders,
+  body: evolveDatesToIso
 });
+
 
 app.post('/github/events', function (req, res) {
-    // Request stream
-    const req$ = rx.Observable.just(req).map(R.pick(['headers', 'body']));
-    
-    // Event stream for all events excluding the push event
-    const eventsWithoutPush$ = req$.filter(R.compose(R.not, isPushEvent));
-    
-    // Github push event stream.
-    const pushEvent$ = req$
-        .filter(isPushEvent)
-        .map(R.compose(evolvePushRequest, getShit))
-        .map(event => R.merge(event.headers, event.body));
-    
-    // All Github events
-    const events$ = rx.Observable.merge(pushEvent$, eventsWithoutPush$);
+  // Request stream
+  const req$ = rx.Observable.just(req).map(R.pick(['headers', 'body']));
 
-    // Send an OK response if all went well
-    events$.subscribe(() => res.send('OK!'), err => res.status(400).send(err.message));
+  // Event stream for all events excluding the push event
+  const eventsWithoutPush$ = req$.filter(R.compose(R.not, isPushEvent));
+
+  // Github push event stream.
+  const pushEvent$ = req$
+    .filter(isPushEvent)
+    .map(R.compose(evolvePushRequest, getShit))
+    .map(event => R.merge(event.headers, event.body));
+
+  // All Github events
+  const events$ = rx.Observable.merge(pushEvent$, eventsWithoutPush$)
+    .doOnNext(event => 
+      exchange$
+        .doOnNext(reply => reply.channel
+        .publish(config.RABBITMQ_EXCHANGE, githubEventType(event), new Buffer(JSON.stringify(event))))
+        .subscribe()
+    );
+
+  // Send an OK response if all went well
+  events$.subscribe(() => res.send('OK!'), err => res.status(400).send(err.message));
 });
 
 /**
  * Start the server
  */
 app.listen(config.PORT, () => {
-    console.log('Express server listening on port ' + config.PORT);
+  console.log('Express server listening on port ' + config.PORT);
 });
 
